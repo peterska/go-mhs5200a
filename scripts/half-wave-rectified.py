78b6872ef5c37a63e163a3cd4f064d640c947cb7
Peter Skarpetis <peters@skarpetis.com>
2021-02-09 11:13:49 +1100

    The rest of the arbitrary waveform generator scripts now take a -p parameter which shows the waveform on screen rather than outputing the data to stdout

--- scripts/half-wave-rectified.py
+++ scripts/half-wave-rectified.py
@@ -3,15 +3,38 @@
 # Generate a half wave rectified waveform to use with the MHS-5200A function generator
 from math import pi
 from math import sin
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, getopt
 
-num_points = 2048
-amplitude = 1.0
-print("# generated  by scripts/half-wave-rectified.py")
+def main(argv):
+    showplot = False
+    try:
+        opts, args = getopt.getopt(argv,"p")
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-p':
+            showplot = True
+    
+    num_points = 2048
+    amplitude = 1.0
+    t = np.linspace(0, 1, 2048)
+    waveform = np.zeros_like(t)
+    # Flags to allow peaks only one point wide
+    for i in range(num_points):
+        x = i/num_points # Fraction along X axis
+        y = amplitude*sin(2*pi*x)
+        if y < 0.0:
+            y = 0.0
+        waveform[i] = y
+    if showplot:
+        plt.plot(t, waveform, '--')
+        plt.show()
+    else:
+        print("# generated  by scripts/half-wave-rectified.py")
+        for y in waveform:
+            print(y)
 
-# Flags to allow peaks only one point wide
-for i in range(num_points):
-    x = i/num_points # Fraction along X axis
-    y = amplitude*sin(2*pi*x)
-    if y < 0.0:
-        y = 0.0
-    print(y)
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- scripts/sinewave-with-spike.py
+++ scripts/sinewave-with-spike.py
@@ -3,24 +3,50 @@
 # example from https://www.bkprecision.com/support/downloads/function-and-arbitrary-waveform-generator-guidebook.html
 from math import pi
 from math import sin
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, getopt
 
-num_points = 2048
-amplitude = 4000
-peak_amplitude = 8000
-print("# generated  by scripts/sinewave-with-spike.py")
+def main(argv):
+    showplot = False
+    try:
+        opts, args = getopt.getopt(argv,"p")
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-p':
+            showplot = True
+    
+    num_points = 2048
+    amplitude = 4000
+    peak_amplitude = 8000
 
-# Flags to allow peaks only one point wide
-positive_done = False
-negative_done = False
-threshold = 1e-4
+    # Flags to allow peaks only one point wide
+    positive_done = False
+    negative_done = False
+    threshold = 1e-4
 
-for i in range(num_points):
-    x = i/num_points # Fraction along X axis
-    y = int(amplitude*sin(2*pi*x))
-    if not positive_done and abs(x - 1/4) < threshold:
-        positive_done = True
-        y = peak_amplitude
-    if not negative_done and abs(x - 3/4) < threshold:
-        negative_done = True
-        y = -peak_amplitude
-    print(y / peak_amplitude)
+    t = np.linspace(0, 1, 2048)
+    waveform = np.zeros_like(t)
+
+    for i in range(num_points):
+        x = i/num_points # Fraction along X axis
+        y = int(amplitude*sin(2*pi*x))
+        if not positive_done and abs(x - 1/4) < threshold:
+            positive_done = True
+            y = peak_amplitude
+        if not negative_done and abs(x - 3/4) < threshold:
+            negative_done = True
+            y = -peak_amplitude
+        waveform[i] = y / peak_amplitude
+
+    if showplot:
+        plt.plot(t, waveform, '--')
+        plt.show()
+    else:
+        print("# generated  by scripts/sinewave-with-spike.py")
+        for y in waveform:
+            print(y)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
